d847b5c11ee861786ebbe0becea75d38d4804c16
AngeloPaolo23 <angelopaolo0223@gmail.com>
2024-07-13 18:33:43 -0400

    ACTUALIZANDO PACKAGE JSON PARA DEPLOY

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const userRoutes = require('./src/routes/userRoutes');
-const productRoutes = require('./src/routes/productRoutes');
-const authRoutes = require('./src/routes/authRoutes');
-const cartRoutes = require('./src/routes/cartRoutes');
-const commentRoutes = require('./src/routes/commentRoutes');
-const favoriteRoutes = require('./src/routes/favoriteRoutes');
+const userRoutes = require("./src/routes/userRoutes");
+const productRoutes = require("./src/routes/productRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+const cartRoutes = require("./src/routes/cartRoutes");
+const commentRoutes = require("./src/routes/commentRoutes");
+const favoriteRoutes = require("./src/routes/favoriteRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`SERVIDOR ENCENDIDO EN PUERTO ${PORT}`);
+  console.log(`SERVIDOR ENCENDIDO EN PUERTO ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('ArtMarket');
+app.get("/", (req, res) => {
+  res.send("ArtMarket");
 });
 
-app.use('/users', userRoutes);
-app.use('/artworks', productRoutes);
-app.use('/auth', authRoutes);
-app.use('/cart', cartRoutes);
-app.use('/comments', commentRoutes);
-app.use('/favorites', favoriteRoutes);
+app.use("/users", userRoutes);
+app.use("/artworks", productRoutes);
+app.use("/auth", authRoutes);
+app.use("/cart", cartRoutes);
+app.use("/comments", commentRoutes);
+app.use("/favorites", favoriteRoutes);
 
 module.exports = app;
