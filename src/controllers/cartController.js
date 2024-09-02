689aa83aa6e0ef1be5ca2c86b8e40599994f92d3
AngeloPaolo23 <angelopaolo0223@gmail.com>
2024-09-01 23:13:58 -0400

    ADDING CONSOLE.ERROR IN CART CONTROLLERS

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,46 +1,65 @@
-const {getProductByID, getCartInfo, addProduct, addOne, sustractOne, getCarts, addOneOrMore, deleteProducts} = require('../models/cart');
+const {
+  getProductByID,
+  getCartInfo,
+  addProduct,
+  addOne,
+  sustractOne,
+  getCarts,
+  addOneOrMore,
+  deleteProducts,
+} = require("../models/cart");
 
 const getAll = async (req, res) => {
-    try {
-        const allCarts = await getCarts();
-        res.json(allCarts);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const allCarts = await getCarts();
+    res.json(allCarts);
+  } catch (error) {
+    console.error("Error al obtener todos los carros:", error);
+    res.status(500).json({ error: error.message });
+  }
 };
 const oneCart = async (req, res) => {
-    try {
-        const products = await getCartInfo(req.params.id);
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const products = await getCartInfo(req.params.id);
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(
+      `Error al obtener el carrito de compras de ID: ${req.params.id}`,
+      error
+    );
+    res.status(500).json({ error: error.message });
+  }
 };
 const add = async (req, res) => {
-    try {
-        const product = await getProductByID(req.body);
-        if (!product) {
-            const newProduct = await addProduct(req.body);
-            res.status(201).json(newProduct);
-        } else {
-            const addingProduct = await addOne(req.body);
-            res.status(201).json(addingProduct);
-        }   
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const product = await getProductByID(req.body);
+    if (!product) {
+      const newProduct = await addProduct(req.body);
+      res.status(201).json(newProduct);
+    } else {
+      const addingProduct = await addOne(req.body);
+      res.status(201).json(addingProduct);
     }
+  } catch (error) {
+    console.error("Error al agregar un producto al carro:", error);
+    res.status(500).json({ error: error.message });
+  }
 };
 const sustract = async (req, res) => {
-    try {
-        const product = await getProductByID(req.body);
-        if (product) {
-            const sustractingProduct = await sustractOne(req.body);
-            deleteProducts(req.body.user_id);
-            res.status(200).json(sustractingProduct);
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const product = await getProductByID(req.body);
+    if (product) {
+      const sustractingProduct = await sustractOne(req.body);
+      deleteProducts(req.body.user_id);
+      res.status(200).json(sustractingProduct);
     }
+  } catch (error) {
+    console.error(
+      "Error al disminuir cantidad de un producto o eliminarlo del carro:",
+      error
+    );
+    res.status(500).json({ error: error.message });
+  }
 };
 
-module.exports = {getAll, oneCart, add, sustract};
+module.exports = { getAll, oneCart, add, sustract };
